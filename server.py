3fc40e8d57257e8f1394bda5360f13e889b46ed8
sychen303 <117107893+sychen303@users.noreply.github.com>
2024-08-27 22:26:37 +0530

    Update server.py

--- server.py
+++ server.py
@@ -8,9 +8,9 @@ latest_axis_data = {}
 @app.route('/test', methods=['POST'])    #for post data
 def receive_data():
     global latest_axis_data
-    data = request.json
+    data = request.json 
     if 'axes' in data:
-        latest_axis_data = data['axes']
+        latest_axis_data = data['axes']  #dictionary with key value pairs
         print(f"Received axis data: {latest_axis_data}")
         return jsonify({"status": "success", "received_axes": latest_axis_data}), 200
     return jsonify({"status": "error", "message": "No axis data received"}), 400
